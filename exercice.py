1ae027ae2632f8e381ca2e70436eceb128de625a
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-09-13 23:36:19 -0400

    implement get_number_of_char

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    pass
+    return string.count(char)
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
