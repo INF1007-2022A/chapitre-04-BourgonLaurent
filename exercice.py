bafc0fb7e0a85b17c5470958f88cd57faefb7d15
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-09-13 23:37:49 -0400

    implement get_number_of_words

--- exercice.py
+++ exercice.py
@@ -23,7 +23,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    pass
+    return sentence.count(word)
 
 
 def main() -> None:
@@ -34,16 +34,24 @@ def main() -> None:
         print(f"Le nombre de caractère dans la chaine {chaine} est impair")
 
     chaine = "salut monde!"
-    print(f"On supprime le 3e caratère dans la chaine: {chaine}. Résultat : {remove_third_char(chaine)}")
+    print(
+        f"On supprime le 3e caratère dans la chaine: {chaine}. Résultat : {remove_third_char(chaine)}"
+    )
 
     chaine = "hello world!"
-    print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
+    print(
+        f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}"
+    )
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_number_of_char(chaine, 'l')}")
-    
+    print(
+        f"Le nombre d'occurrence de l dans {chaine} est : {get_number_of_char(chaine, 'l')}"
+    )
+
     chaine = "Baby shark doo doo doo doo doo doo"
-    print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
+    print(
+        f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
